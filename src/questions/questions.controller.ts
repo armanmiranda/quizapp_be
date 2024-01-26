f875987529b49461edbd507e3a738f206decb3c5
Arman Miranda <arman@localhost.localdomain>
2024-01-26 22:08:49 +0800

    feat: add implementation for getting question data

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 
 @Controller('questions')
 export class QuestionsController {
   constructor(private questionsService: QuestionsService) {}
 
+  @Get('/')
+  index() {
+    return this.questionsService.index();
+  }
+
   @Get('/:id')
-  show() {
-    return {};
+  show(@Param('id', ParseIntPipe) id: number) {
+    return this.questionsService.show(id);
   }
 }
--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -4,4 +4,36 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable({})
 export class QuestionsService {
   constructor(private prisma: PrismaService) {}
+
+  async index() {
+    try {
+      const questions = await this.prisma.question.findMany();
+      return { status: 200, data: questions };
+    } catch (e) {
+      console.log(e);
+      return { status: 500, data: [] };
+    }
+  }
+
+  async show(id: number) {
+    try {
+      const questions = await this.prisma.question.findUniqueOrThrow({
+        where: { id },
+        select: {
+          id: true,
+          question: true,
+          answers: {
+            select: {
+              id: true,
+              answer: true,
+            },
+          },
+        },
+      });
+      return { status: 200, data: questions };
+    } catch (e) {
+      console.log(e);
+      return { status: 500, data: [] };
+    }
+  }
 }
