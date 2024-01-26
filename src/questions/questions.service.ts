83fca5cb3e0b821917f55f2e52907dbb4b9a20fa
Arman Miranda <arman@localhost.localdomain>
2024-01-27 00:30:31 +0800

    feat: minimize questions all response
    Minimize Question's response to a few field then added Answer as one of
    its fields

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -8,7 +8,15 @@ export class QuestionsService {
 
   async index() {
     try {
-      const questions = await this.prisma.question.findMany();
+      const questions = await this.prisma.question.findMany({
+        select: {
+          id: true,
+          question: true,
+          answers: {
+            select: { id: true, answer: true },
+          },
+        },
+      });
       return { status: 200, data: questions };
     } catch (e) {
       this.logger.error(e);
@@ -24,10 +32,7 @@ export class QuestionsService {
           id: true,
           question: true,
           answers: {
-            select: {
-              id: true,
-              answer: true,
-            },
+            select: { id: true, answer: true },
           },
         },
       });
